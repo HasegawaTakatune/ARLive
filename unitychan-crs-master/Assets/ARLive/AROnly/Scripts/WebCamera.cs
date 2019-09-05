93e472d2e00d351f11f212fb523673ece1e03c0e
cDGJ3vgijc <m.touyama@eaz.co.jp>
2019-09-05 18:07:56 +0900

    WebCameraから画像を取得してマテリアルに割り当てる処理を追加

--- unitychan-crs-master/Assets/ARLive/AROnly/Scripts/WebCamera.cs
+++ unitychan-crs-master/Assets/ARLive/AROnly/Scripts/WebCamera.cs
@@ -1,43 +1,60 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class WebCamera : MonoBehaviour
 {
 
-    private int width = 1920;
-    private int height = 1080;
+    [SerializeField] private Material outMaterial;
 
-    [SerializeField] private RawImage display;
+    private WebCamDevice[] webCamDevices = null;
 
-    private WebCamTexture webCamTexture = null;
+    private int selectCamera = 0;
 
+    /// <summary>
+    /// 幅
+    /// </summary>
+    private int Width = 1920;
+
+    /// <summary>
+    /// 高さ
+    /// </summary>
+    private int Height = 1080;
+
+    /// <summary>
+    /// フレームレート
+    /// </summary>
+    private int FPS = 30;
+
+    /// <summary>
+    /// Webカメラテクスチャ
+    /// </summary>
+    private WebCamTexture webCamTexture;
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
     private IEnumerator Start()
     {
-        // カメラデバイスの存在確認
-        if (WebCamTexture.devices.Length == 0)
+        int count = 0;
+        while (true)
         {
-            Destroy(this);
-            yield break;
+            try
+            {
+                if (count >= 100) break;
+                count++;
+                webCamDevices = WebCamTexture.devices;
+                if (webCamDevices.Length > 0) break;
+            }
+            catch { }
+            yield return new WaitForSeconds(Time.deltaTime);
         }
 
-        // カメラデバイスの使用許可を求める
-        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
-
-        // カメラデバイスの使用許可が下りなかった場合
-        if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
+        if (webCamDevices != null && webCamDevices.Length > 0)
         {
-            Destroy(this);
-            yield break;
+            webCamTexture = new WebCamTexture(webCamDevices[selectCamera].name, Width, Height, FPS);
+
+            outMaterial.mainTexture = webCamTexture;
+            webCamTexture.Play();
         }
-
-        WebCamDevice webCamDevice = WebCamTexture.devices[0];
-        webCamTexture = new WebCamTexture(webCamDevice.name, width, height);
-    }
-
-    void Update()
-    {
-
     }
 }
